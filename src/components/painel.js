3072202ba60e81c157425d4d7e37af28eefd6872
fexx <fexx@hotmail.co.uk>
2017-11-06 21:11:31 -0200

    Criando componentes Entrada, Operacao e Comando

--- src/components/painel.js
+++ src/components/painel.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import Entrada from './entrada';
+import Operacao from './operacao';
+import Comando from './comando';
+
 const Painel = props => (
     <View>
-        <Text>Painel</Text>
+        <Entrada/>
+        <Operacao/>
+        <Comando/>
     </View>
 );
 export { Painel };
